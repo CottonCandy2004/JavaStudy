9fa8891dae52e613e94a9ab709656678791dcb8f
CottonCandy2004 <CandySanjo@outlook.com>
2025-03-10 18:03:32 +0800

    update lesson3

---
+++ src/Class/Lesson3/Hanio.java
@@ -0,0 +1,24 @@
+package Class.Lesson3;
+
+import java.util.Scanner;
+
+public class Hanio {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please input the number of disks:");
+        int n = sc.nextInt();
+        System.out.println("The steps to move " + n + " disks:");
+        hanio(n, 'A', 'B', 'C');
+        sc.close();
+    }
+
+    public static void hanio(int n, char a, char b, char c) {
+        if (n == 1) {
+            System.out.printf("Move disk %d from %c to %c\n", n, a, c);
+        } else {
+            hanio(n - 1, a, c, b);
+            System.out.printf("Move disk %d from %c to %c\n", n, a, c);
+            hanio(n - 1, b, a, c);
+        }
+    }
+}
